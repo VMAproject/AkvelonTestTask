92802c0e89c85db734319b693a5a194795eea910
Verlamov <verlamov.m@gmail.com>
2017-02-24 18:32:00 +0200

    format dataDB

---
+++ src/main/resources/dataDB.sql
@@ -0,0 +1,9 @@
+INSERT INTO akvelon.contacts (firstname, lastname, telephone, email, created, rating) VALUES
+('Vasya', 'Dutka', 0631264154, 'vasya@mail.com', '2017-03-22 20:20:44.0', 12.9),
+('Jack', 'London', 0631264153, 'jl@mail.en', '2016-03-23 17:34:46.0', 4.54),
+('Obama', 'Barak', 0631264159, 'bo@mail.ua', '1817-03-22 16:49:36.0', 3.8),
+('Met', 'Deamon', 0631264354, 'Met@s.ty', '1975-03-22 18:28:29.0', 25.5),
+('Stepan', 'Bandera', 063124154, 'stepan@lvov.ua', '1909-01-01 18:53:43.0', 1000.0),
+('Vin', 'Diesel', 0631674154, 'Diesel@cheap.en', '2300-03-22 18:41:08.0', 0.5),
+('Masha', 'Antonova', 0631264150, 'masha@love.com',x '2015-03-22 21:03:27.0', 36.6),
+('Elisabet', 'Tetcher', 0731264154, 'eli@mail.com', '2015-03-23 19:01:05.0', 2.8);
